bdfb76a6b570f2d56dc66de68f6183c046abfc51
Igor Liberato <igcbsb@gmail.com>
2023-02-20 18:34:29 +0000

    tp1

---
+++ tp1/MyParallelogram.js
@@ -0,0 +1,43 @@
+import {CGFobject} from '../lib/CGF.js';
+/**
+ * MyTriangle
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyParallelogram extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	
+	initBuffers() {
+		this.vertices = [
+			0, 0, 0,	//0
+			1, 0, 0,	//1
+			1, 1, 0,	//2
+			2, 0, 0,	//3
+			2, 1, 0,	//4
+			3, 1, 0,	//5
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			0, 1, 2,
+			2, 1, 3,
+			3, 4, 2,
+			3, 5, 4,
+
+			2, 1, 0,
+			3, 1, 2,
+			2, 4, 3,
+			4, 5, 3,
+		];
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+}
+
